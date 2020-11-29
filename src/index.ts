858e91ae3fd0688751446c0a29279c6a5a5b821e
shadowtime2000 <66655515+shadowtime2000@users.noreply.github.com>
2020-11-29 12:13:26 -0800

    docs: added jsdoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/**
+ * Creates an HTML string
+ * @param tag
+ * @param props
+ * @param children
+ * 
+ * Examples
+ * ```typescript
+ * l("div", { id: "mydiv", className: "amazingNess" }, [
+ *  l("button", { className: "btn btn--dark" }, "Click Me!"),
+ *  l("p", { className: "text text--secondary" }, "Yeah do it!"),
+ *  "No don't do it!"
+ * ])
+ * ```
+ */
 const l = (tag: string, props: Record<string, string> = {}, children: string[] | string = []) => {
   const propsToUse = Object.keys(props).filter((prop) => prop !== 'className');
   let final = `<${tag}${propsToUse.map((prop) => ` ${prop}='${props[prop]}'`).join('')}`;
