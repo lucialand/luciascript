b125c44a2810a1310286b0e2076e62ee1bafbcb6
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-03-01 20:49:44 +0530

    Added class support

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,50 @@
+
+class L{
+  private readonly tagName:string;
+  private props:Record<string, string>;
+  private children:Array<string> | string;
+
+  /**
+   * 
+   * @param tag
+   * @param props 
+   * @param children 
+   */
+  constructor(readonly tag:string, props:Record<string, string> = {}, children:Array<string> | string = []) {
+    this.tagName = tag
+    this.props = props
+    this.children = children
+
+  }
+
+  /**
+   * Generate the html string
+   */
+  public create = ():string => {
+    const propsToUse = this.ignoreObjectClassNames(this.props)
+
+    let htmlContent:string = `<${this.tagName}${propsToUse.map((prop) => ` ${prop}='${this.props[prop]}'`).join('')}`;
+
+    htmlContent = this.addObjectClassName(htmlContent, this.props)
+
+    htmlContent = `${htmlContent}>${typeof this.children === 'string' ? this.children : this.children.join('')}</${this.tagName}>`;
+    return htmlContent
+  }
+
+  private addObjectClassName(content:string, props:Record<string, string>){
+    if (props.className) {
+      content = `${content} class='${props.className}'`
+    }
+
+    return content
+  }
+
+  
+  private ignoreObjectClassNames(props:Record<string, string>){
+    return Object.keys(props).filter((prop) => prop !== 'className');
+  }
+}
+
 /**
  * Creates an HTML string
  * @param tag
@@ -13,6 +60,8 @@
  * ])
  * ```
  */
+
+
 const l = (tag: string, props: Record<string, string> = {}, children: string[] | string = []) => {
   const propsToUse = Object.keys(props).filter((prop) => prop !== 'className');
   let final = `<${tag}${propsToUse.map((prop) => ` ${prop}='${props[prop]}'`).join('')}`;
@@ -21,4 +70,5 @@ const l = (tag: string, props: Record<string, string> = {}, children: string[] |
   return final;
 };
 
-export { l };
+
+export { l, L };
